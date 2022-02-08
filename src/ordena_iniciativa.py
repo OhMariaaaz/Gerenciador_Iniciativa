8448d3be2ff08592f36d025ed79667304f53cad9
JulioGoes <goes.cesar157@gmail.com>
2022-02-08 15:21:18 -0300

    update

--- src/ordena_iniciativa.py
+++ src/ordena_iniciativa.py
@@ -1,15 +1,7 @@
-lista_de_nomes = ['Júlio', 'Eduarda', 'Luiz']
-lista_de_numeros = [20, 20, 13]
-cond_life = 'Vivo'
-cond_life_2 = 'Morto'
-condicao = True
-dici = {}
-lista = []
+def ordena(nome, iniciativa):
+    dici = {}
+    for i in range(len(nome)):
+        dici.update({nome[i]: iniciativa[i]})
 
-for i in range(3):
-    dici.update({lista_de_nomes[i]: lista_de_numeros[i]})
-
-
-print('ORDEM DA INICIATIVA')
-for item in dici.items():
-    print(item[1], '-', item[0])
+    for item in dici.items():
+        print(item[1], '-', item[0])
