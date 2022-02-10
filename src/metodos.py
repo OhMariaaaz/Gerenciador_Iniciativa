d0ec8628c4fbf78f348ad9945194816797ad71e5
JulioGoes <goes.cesar157@gmail.com>
2022-02-10 20:38:18 -0300

    atualização final do código

--- src/metodos.py
+++ src/metodos.py
@@ -24,23 +24,57 @@ def ordena(nome, iniciativa):
 def imprimi_na_tela(dicionario, contador):
     count = 0
     turno = 0
-    rodada = 0
+    rodada = 1
     condicao = True
+
+    # O loop a seguir mantém o gerenciador de iniciativa ativo até o usuário
+    # desejar cancelar
     while condicao is True:
+
+        # A mensagem a seguir imprimi a rodada atual
+        # que é incrimentada sempre que uma rodada é concluída
+        if turno == len(dicionario.values()):
+            rodada += 1
         print('RODADA {}'.format(rodada))
 
+        # O loop a seguir imprimi na tela o nome e o valor de iniciativa
+        # de todos os personagens adicionados ao combate, além disso,
+        # coloca uma marcação '>' que funciona como um indicador de quem
+        # é a vez de jogar caso a rodada seja concluída, o indicador retorna
+        # para o começo da iniciativa.
         for item in dicionario.items():
+
+            # A variavel 'turno' é usada para indicar onde o marcador deve ser
+            # inserido ela começa no indice 0 da lista contador, e quando chega
+            # ao final da lista reseta para o zero
             if turno == len(dicionario.values()):
                 turno = 0
-                rodada += 1
-            # elif turno == (len(dicionario.values())):
+
+            # A lista recebe a marcação para servir de indicador, após isso
+            # imprimi na tela o indicador, o nome e o valor de iniciativa da
+            # pessoa. Além disso, é também impresso na tela todos os outros
+            # personagens da iniciativa, e caso não seja a vez dele de jogar
+            # é impresso o indicador '_'
             contador[turno] = '>'
             print(contador[count], item[1], '-', item[0])
             contador[turno] = '_'
+
+            # O count é usado para imprimir todos os valores da lista
+            # 'contador' de maneira incremental. Lembrando que: caso
+            # seja o turno do personagem de jogar, será exibido o
+            # indicador '>'
             count += 1
+
+        # Após impresso as informações referentes ao primeiro turno
+        # do combate, a variável count é resetada, e a variável turno
+        # é incrimentada
         count = 0
         turno += 1
 
+        # Cada vez que o usuário digiar 1 (Sim), o Loop irá imprimir
+        # novamente indicador de turno, nome e iniciativa, quando
+        # o loop percorrer por todos os personagens, uma rodada
+        # é concluída
         print('')
         msg = 'Proximo Turno:'
         condicao = confirma(msg)
